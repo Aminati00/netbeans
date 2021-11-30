26781c5a8f5e748a7c56412adc30c015cd9363c2
143sc <143sc@DESKTOP-G5BOOQV.speedport.ip>
2021-11-30 20:09:10 +0100

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/adressenverwaltung/controller/commands/CommandInvoker.java
+++ src/adressenverwaltung/controller/commands/CommandInvoker.java
@@ -12,23 +12,38 @@ import java.util.Stack;
 /**
  *
  * @author marco
+ * Invoker für Command Design Pattern
+ * 
  */
 public class CommandInvoker 
 {
   private HashMap<Object, CommandInterface> commands;
   private Stack<CommandInterface> undoStack;
   
+  /**
+   * Konstruktor erstellt eine Hashmap und einen UndoStack
+   */
   public CommandInvoker()
   {
     commands = new HashMap<>();
     undoStack = new Stack<>();
   }
   
+  /**
+   * Funktion addCommand() weist Objekt zu Kommado zu
+   * @param key Referenz auf Eventquelle
+   * @param value 
+   */
   public void addCommand(Object key, CommandInterface value)
   {
     commands.put(key, value);
   }
   
+  
+  /**
+   * Funktion executeCommand() ruft Funktion für das jeweilige Objekt auf
+   * @param key 
+   */
   public void executeCommand(Object key)
   {
     commands.get(key).execute();
@@ -37,6 +52,7 @@ public class CommandInvoker
       undoStack.push(commands.get(key));
     }
   }
+  
   
   public void undoCommand()
   {
